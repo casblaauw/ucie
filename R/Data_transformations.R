4e6d4d13ea211dd9a6ae1330ed405218e0a3ac5b
Cas Blaauw <38132585+CasBlaauw@users.noreply.github.com>
2022-03-01 16:12:31 +0100

    Add prep_ucie_data matrix-making fn from phosphocie

--- R/Data_transformations.R
+++ R/Data_transformations.R
@@ -53,7 +53,98 @@ RGB_space <- as.matrix(data.frame(
          rep(255, 9))# M -> W
 ))
 
+#' Check and reformat data for use with U-CIE functions
+#'
+#' Takes a 2D or 3D matrix or data frame and returns a 3D matrix, checked for non-numeric values.
+#'
+#' @param dataset A data frame or matrix of 2D or 3D values.
+#' @param rownames_col Optional: name of column to ignore during calculations.
+#' @param check_3D Optional: whether to check and force 3D output. Needed for reduced/cielab data,
+#' not needed for KNN data.
+#'
+#' @details
+#'   The function does a number of processing steps to accept as many data formats as feasible:
+#'   * Rownames in the first column of the matrix (as tested by unconvertability to numeric) or of the data frame are discarded.
+#'   * Character matrices are converted to numeric.
+#'   * Data frames are converted to matrices.
+#'   * If rownames_col is given, that column is discarded as well.
+#'   * 2D matrices or data frames are expanded to 3D by padding with 1's.
+#'
+#' @keywords internal
+prep_ucie_data <- function(dataset, rownames_col = NULL, check_3D = TRUE) {
 
+  if (!is.null(rownames_col)) {
+    if (ncol(dataset) < 3) {
+      rlang::abort('Your data needs to be at least 2D to use `rownames_col`.')
+    }
+    if (inherits(dataset, "data.frame")) {
+      dataset <- tibble::column_to_rownames(dataset, rownames_col)
+    } else if (inherits(dataset, 'matrix')) {
+      rownames_temp <- dataset[,rownames_col]
+      dataset <- dataset[,which(colnames(dataset) != rownames_col), drop = FALSE]
+      rownames(dataset) <- unname(rownames_temp)
+    }
+  }
+
+  # If data frame, transform to matrix
+  if (inherits(dataset, "data.frame")) {
+    # If rownames column supplied or first column looks like rownames, move to actual rownames
+    if (is.character(dataset[[1]]) & is.null(rownames_col)) {
+      rownames(dataset) <- dataset[[1]]
+      dataset <- dataset[[-1]]
+    }
+
+    # Check for non-numeric data
+    if (!all(purrr::map_lgl(dataset, is.numeric))) {
+      rlang::abort(paste("The dataset contains non-numeric columns.",
+                         "Please check your data and use the rownames_col argument if necessary."))
+    }
+    dataset <- as.matrix(dataset)
+  }
+
+  # If rownames encoded in first matrix row, move to true rownames if not present or drop
+  if (anyNA(as.numeric(dataset[,1]))) {
+    val <- dataset[,1]
+    dataset <- dataset[,-1]
+    if (is.null(rownames(dataset))) {
+      rownames(dataset) <- val
+    }
+  }
+
+  # If character matrix, transform into numeric
+  if (!all(is.numeric(dataset))) {
+    class(dataset) <- 'numeric'
+  }
+
+  # Check for missing/leftover character data
+  if (anyNA(dataset)) {
+    rlang::abort(paste("Your matrix data contains missing data or",
+                       "non-numeric data outside the first column.",
+                       "Please check your dataset."))
+  }
+
+  if (check_3D) {
+
+    # Check data dimensionality
+    if (ncol(dataset) == 2) {
+      warning("Data expanded to 3D!")
+      dataset <- cbind(dataset, 1)
+    }
+    if (ncol(dataset) != 3) {
+      rlang::abort("The dataset should have 3 numeric columns!")
+    }
+
+    # Check for missing values
+    if (anyNA(dataset)) {
+      rlang::abort("The dataset has missing values. Check again!")
+    }
+
+    # Set column names
+    colnames(dataset) <- c('X', 'Y', 'Z')
+  }
+
+  return(dataset)
+}
 
 ColorSpacePolygon <- function(ColorSpace){
   RGB2Lab <- function(mat) {
